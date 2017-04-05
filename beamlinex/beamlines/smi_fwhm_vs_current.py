6b90f8017341f1fb031c548befd0ec67ac92aace
Maksim Rakitin <maxim.rakitin@gmail.com>
2017-04-04 20:07:12 -0400

    SMI - updated FWHM vs. current plotting.

--- beamlinex/beamlines/smi_fwhm_vs_current.py
+++ beamlinex/beamlines/smi_fwhm_vs_current.py
@@ -3,20 +3,42 @@ from matplotlib import pyplot as plt
 
 from beamlinex.common.databroker import read_single_scan
 from beamlinex.common.io import save_data_numpy
+from beamlinex.common.plot import clear_plt
 
 
-def smi_fwhm_vs_current(reverse=False):
-    remove = 529
-    first = 530  # 418
-    last = 560
-    scans = list(range(first, remove)) + list(range(remove + 1, last + 1))
+def smi_fwhm_vs_current(reverse=False, current='mean', show=True):
+    allowed_current_values = ('mean', 'peak', 'first', 'last')
+    if current not in allowed_current_values:
+        raise ValueError('{}: not allowed. Allowed values: {}'.format(current, allowed_current_values))
+
+    exclude = [529] + list(range(565, 584))
+
+    first = 418
+    last = 657
+
+    scans = []
+    for i in range(first, last + 1):
+        if not i in exclude:
+            scans.append(i)
+
+    print('Scans list:\n{}'.format(scans))
+
     fwhm_vs_current = np.zeros((len(scans), 2))
     fname = 'smi_fwhm_vs_current_{}_to_{}'.format(scans[0], scans[-1])
     for i, s in enumerate(scans):
         print('s={}'.format(s))
         d = read_single_scan(s, x_label='dcm_bragg', y_label='VFMcamroi1')
         fwhm = d['fwhm']
-        ring_current = list(d['data']['ring_current'])[-1]
+        if current == 'mean':
+            ring_current = np.mean(d['data']['ring_current'])
+        else:
+            if current == 'peak':
+                idx = d['data']['VFMcamroi1'].argmax()
+            elif current == 'first':
+                idx = 0
+            elif current == 'last':
+                idx = -1
+            ring_current = list(d['data']['ring_current'])[idx]
         fwhm_vs_current[i, 0] = ring_current
         fwhm_vs_current[i, 1] = fwhm
 
@@ -30,18 +52,30 @@ def smi_fwhm_vs_current(reverse=False):
     # Plotting:
     plt.figure(figsize=(16, 10))
     plt.grid()
-    plt.xlabel('Ring current [mA]')
+    plt.xlabel('Ring current [mA] (current={})'.format(current))
     plt.ylabel('FWHM [deg]')
     if reverse:
         plt.xlim(fwhm_vs_current[0, 0], fwhm_vs_current[-1, 0])
-    plt.plot(fwhm_vs_current[:, 0], fwhm_vs_current[:, 1])
+    plt.scatter(fwhm_vs_current[:, 0], fwhm_vs_current[:, 1], s=200)
     plt.tight_layout()
-    plt.savefig('{}.png'.format(fname))
-    plt.show()
-
+    plt.savefig('{}_current={}.png'.format(fname, current))
+    if show:
+        plt.show()
+    clear_plt()
     print('')
 
 
 if __name__ == '__main__':
     reverse = False
-    smi_fwhm_vs_current(reverse)
+
+    # current = 'mean'
+    current = 'peak'
+    # current = 'first'
+    # current = 'last'
+
+    show = True
+
+    # for current in ('mean', 'peak', 'first', 'last'):
+    #     smi_fwhm_vs_current(reverse=reverse, current=current, show=show)
+
+    smi_fwhm_vs_current(reverse=reverse, current=current, show=show)
