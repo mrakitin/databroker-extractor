97c6c3436f72e83a424ed4eaa9afa41975da2d8e
Maksim Rakitin <maxim.rakitin@gmail.com>
2017-09-12 15:25:13 -0400

    SMI - updated code for 17th harmonic scan.

--- databroker_extractor/beamlines/smi_harmonic_scans.py
+++ databroker_extractor/beamlines/smi_harmonic_scans.py
@@ -11,8 +11,13 @@ def normalize(y, shift=0.0):
 if __name__ == '__main__':
     d = dbe.activate_beamline_db('smi')
 
-    a = dbe.read_single_scan(d, 310)
-    b = dbe.read_single_scan(d, 929)
+    # 7th harmonic:
+    # a = dbe.read_single_scan(d, 310)
+    # b = dbe.read_single_scan(d, 929)
+
+    # 17th harmonic:
+    a = dbe.read_single_scan(d, 306)
+    b = dbe.read_single_scan(d, 930)
 
     x_a = a['data']['bragg']
     y_a = a['data']['VFMcamroi1']
@@ -20,7 +25,11 @@ if __name__ == '__main__':
     y_b = b['data']['FScamroi4']
     x_b = b['data']['energy_bragg']
 
-    diff = (float(x_a[np.where(y_a == y_a.max())[0]]) - float(x_b[np.where(y_b == y_b.max())[0]])) * 1.2
+    # 7th harmonic:
+    # diff = (float(x_a[np.where(y_a == y_a.max())[0]]) - float(x_b[np.where(y_b == y_b.max())[0]])) * 1.2
+
+    # 17th harmonic:
+    diff = (float(x_a[np.where(y_a == y_a.max())[0]]) - float(x_b[np.where(y_b == y_b.max())[0]])) * 1.0
 
     plt.plot(x_b, normalize(y_b), label='scan {}'.format(b['scan']['start']['scan_id']))
     plt.plot(x_a - diff, normalize(y_a), label='scan {}'.format(a['scan']['start']['scan_id']))
